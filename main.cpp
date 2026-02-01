e99cde0363b2092bf826bc35d4214017663e4a57
Paul <usuikagedestiny@gmail.com>
2026-01-31 18:54:32 -0800

    Add student record system with menu and functions
    Implement a student record system with functions for managing student grades.

---
+++ main.cpp
@@ -0,0 +1,121 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+// This is global variable, do not modify
+#define MAX_STUDENTS 5
+
+struct Student {
+	int studentId;
+	string studentName;
+	float studentGrades;
+};
+
+/*!
+* Function Prototypes
+*/
+void printMenu();
+void highestGrade(Student students[], int count);
+void lowestGrade(Student students[], int count);
+void averageGrade(Student students[], int count);
+void printStudentId(Student students[], int count);
+void printStudentname(Student students[], int count);
+
+int main() {
+	Student students[MAX_STUDENTS];
+	int count = 0;  // this keeps track of the students that are in the class
+	int choice;     // user input
+
+	/*!
+	* Add the students here!
+	*/
+	//----------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+	//----------------------------------------------------------------------------------------
+
+	// Complete the menu with the associated function
+	do {
+		printMenu();
+		cout << "Enter your choice: ";
+		cin >> choice;
+		cin.ignore();  // we add this to consume the leftover newline
+
+		switch (choice) {
+		case 1:
+			highestGrade(students, count);
+			break;
+		case 2:
+			lowestGrade(students, count);
+			break;
+		case 3:
+			averageGrade(students, count);
+			break;
+		case 4:
+			printStudentId(students, count);
+			break;
+		case 5:
+			printStudentname(students, count);
+			break;
+		case 6:
+			cout << "Goodbye!\n";
+			break;
+		default:
+			cout << "Invalid choice. Please try again.\n";
+		}
+	} while (choice != 6);
+
+	return 0;
+}
+
+void printMenu() {
+	cout << "\n=== Student Record System ===\n";
+	cout << "1. Find student with the highest grade\n";
+	cout << "2. Find the student with the lowest grade\n";
+	cout << "3. Search student by roll number\n";
+	cout << "4. Update student grades\n";
+	cout << "5. Exit\n";
+}
+
+/* TODO: Implement these functions */
+
+// Remeber, if you pass an array into a function, it is a pass by reference!
+// Because arrays decay to poitners, so you are getting a pointer to the first element
+// which allows you to modify the data. 
+
+void highestGrade(Student students[], int count)
+{
+	// TODO: Print out all the highest grade in the array
+
+	// Here is a simple check to ensure your code does not break
+	if (count == 0) {
+		cout << "No students in the record.\n";
+		return;
+	}
+}
+void lowestGrade(Student students[], int count)
+{
+	// TODO: Print out all the lowest grade in the array
+}
+
+void averageGrade(Student students[], int count)
+{
+	// TODO: Print out the average grade of the students
+}
+void printStudentId(Student students[], int count)
+{
+	// TODO: Print out all the students id in the array
+}
+void printStudentname(Student students[], int count)
+{
+	// TODO: Print out all the students name in the array
+}
